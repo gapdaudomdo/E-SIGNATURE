afb0304c66140cc820ec0030aaa77431da0000f6
giahuy <gapdaudomdo01@gmail.com>
2022-10-06 20:10:20 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
+import Popup from 'reactjs-popup';
+import SignaturePad from 'react-signature-canvas';
 import './App.css';
+import "./sigCanvas.css"
+
+
 
 function App() {
+  const [imageURL, setImageURL] =useState(null);
+
+  const sigCanvas = useRef({});
+
+  const clear = () => sigCanvas.current.clear();
+
+  const save = () => 
+    setImageURL(sigCanvas.current.getTrimmedCanvas().toDataURL("image/png"));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cf-title-01">
+    <h1> E-Signature </h1>
+    </div>
+      <Popup modal trigger={<button>Box</button>}
+      closeOnDocumentClick={false}
+      >
+      {close => (
+        <>
+          <SignaturePad
+            ref={sigCanvas}
+            canvasProps={{
+              className: "signatureCanvas"
+            }}
+          />
+          <br/>
+          <button onClick={save}>Save</button>
+          <button onClick={clear}>Clear</button>
+          <button onClick={close}>Close</button>
+        </>
+      )}
+      </Popup>
+      <br />
+      <br />
+      {imageURL ? (
+        <img
+          src={imageURL}
+          alt="my signature"
+          style={{
+            display: "block",
+            margin: "0 auto",
+            border: "1px solid black",
+            width: "150px" 
+        }}
+        />
+      ) : null}
+
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
